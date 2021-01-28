c057347ca43794c6f83dc519655e1141a444f1c2
Voevodskiy Egor <voevodskiy.ea@gmail.com>
2021-01-28 21:30:48 +0300

    report

--- client/src/components/InputFile/InputFile.js
+++ client/src/components/InputFile/InputFile.js
@@ -19,50 +19,70 @@ function InputFileFunc() {
     function onDropHandler(e) {
         e.preventDefault()
         let file = e.dataTransfer.files[0]
-        setUpload(file)
+        validateInput(file.name)
         setDrag(false)
-    }
-
-    function handleFile(event) {
-        let file = event.target.files[0];
         setUpload(file)
     }
 
-    function handleUpload(event) {
+    function handleUpload(e) {
         console.log(upload)
+        /// PFUHEP[RF]
     }
+
+    function validateInput (e){
+        if (e.name.split(".")[e.name.split(".").length - 1] !== "txt") {
+            throw Error("wrong file format");
+          } else {
+            console.log("Success")
+          }
+          if (e.size > 1024 * 1024 * 10) {
+            throw Error("file size is bigger then 10Mb")
+          }
+    }
+    
     return (
         <div className="input unselectable">
             <div className="input__content">
-                {upload ?   <div className="loaded">
-                                <Close onClick={() => setUpload(undefined)} className="loaded__cancel" />
-                                <div className="loaded__name">
-                                    {upload.name}
-                                </div>
+                {upload ?
+                        //Файл заружен
+                        <div className="loaded">
+                            <Close onClick={() => setUpload(undefined)} className="loaded__cancel" />
+                            <div className="loaded__name">
+                                {upload.name}
                             </div>
+                        </div>
                         : drag
-                        ? <div
+                        //Дрoп файла
+                        ? 
+                        <div
                             onDragStart={e => dragStartHandler(e)}
                             onDragLeave={e => dragLeaveHandler(e)}
                             onDragOver={e => dragStartHandler(e)}
                             onDrop={e => onDropHandler(e)}
-                            className="waiting">
+                            className="waiting-drop">
                             <Download className="waiting__icon" />
-                            <label className="waiting__label">Drop file</label> 
+                            <label className="waiting__label3">Drop to upload</label> 
                         </div>
-                        : <div
+                        //oснoвнoй
+                        : 
+                        <div
                             onDragStart={e => dragStartHandler(e)}
                             onDragLeave={e => dragLeaveHandler(e)}
                             onDragOver={e => dragStartHandler(e)}
                             className="waiting">
                             <Download className="waiting__icon" />
-                            <label htmlFor="file" className="waiting__label">Choose a file</label>
+                            <label className="waiting__label3">Drag file here</label>
+                            <div className="testF"> 
+                                <label className="waiting__label2">Or </label>
+                                <label htmlFor="file" className="waiting__label1"> choose a file</label>
+                            </div>
+                            
                         </div>}
             </div>
-            <div className="input__upload">
-                <input type="file" id="file" name="file" className="display-none" onChange={(event) => { handleFile(event) }} />
-                <div onClick={(event) => handleUpload(event)}>Upload</div>
-            </div>
+            {!upload ? 
+            <input type="file" id="file" name="file" className="display-none" onChange={(e) => { setUpload(e.target.files[0]); validateInput(e.target.files[0].name) }} />
+            : ""}
+            <button className="input__upload" onClick={(e) => handleUpload(e)} disabled={upload ? false : true} >Upload</button>
         </div>
     )
 }
